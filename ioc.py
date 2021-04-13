3e21b8409053537f08a3de9f4d8237bb2bb164f9
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2021-04-13 16:35:05 -0400

    use ipaddress for ip validation

--- ioc.py
+++ ioc.py
@@ -1,56 +1,39 @@
 """ IOC finding functions """
+
 import re
+
+from ipaddress import AddressValueError, IPv4Address, IPv6Address
 from typing import Mapping, List
 
-IPV4_RE = rb'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
-IPV6_RE = rb'\b((?:[\da-f]{1,4}:){7,7}[\da-f]{1,4}|(?:[\da-f]{1,4}:){1,6}(:|(?::[\da-f]{1,4}){1,5}))\b'
-DOMAIN_RE = rb'\b(?:[A-Z0-9-]+\.)+(?:XN--[A-Z0-9]{4,18}|[A-Z]{2,12})\b'
+from string_helper import make_str
+
+IP_RE = rb'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+DOMAIN_RE = rb'\b(?:[a-z0-9-]+\.)+(?:xn--[a-z0-9]{4,18}|[a-z]{2,12})\b'
 URI_RE = rb'(?:ftp|http|https)://' \
-         rb'[A-Z0-9.-]{1,}\.(?:XN--[A-Z0-9]{4,18}|[a-z]{2,12}|[0-9]{1,3})' \
+         rb'[a-z0-9.-]+\.(?:xn--[a-z0-9]{4,18}|[a-z]{2,12}|[0-9]{1,3})' \
          rb'(?::[0-9]{1,5})?' \
-         rb'(?:/[A-Z0-9/\-\.&%\$#=~\?_+]{3,200}){0,1}'
+         rb'(?:/[a-z0-9/\-\.&%\$#=~\?_+]{3,200})?'
 
-def valid_ipv4(ip: bytes) -> bool:
-    """ Validates an ip address """
+def is_public_ip(ip: Union[str, bytes]) -> bool:
+    """
+    Checks if an ipv4 address is valid and a standard public internet address
+
+    rejects invalid addresses.
+    rejects a valid addresses if it is:
+        - a multicast address
+        - a private addresses
+        - a reserved addresses
+    """
     try:
-        address = [int(n) for n in ip.split(b'.')]
-    except ValueError:
+        address = ipaddress.IPv4Address(make_str(ip))
+    except AddressValueError:
         return False
-    if (len(address) != 4 # wrong format
-            or any(x > 255 for x in address) # out of ranage
-            or address[0] == 0               # invalid destination address
-            or address[3] in (0, 255)):      # ignore network identifier and broadcast addresses
-        return False
-    return True
-
-def reserved_ipv4(ip:bytes) -> bool:
-    """ Checks if a valid ip address is in a reserved address space"""
-    address = [int(n) for n in ip.split(b'.')]
-    assert len(address) == 4
-
-    return address[0] in ( # /8 subnets
-            0,  # current network (invalid destination)
-            10, # private network
-            127 # loopback
-        ) or (address[0], address[1]) in ( # /16 subnets
-            (192, 168), # private network
-            (169, 254)  # link-local
-        ) or (address[0], address[1], address[2]) in ( # /24 subnets
-            (192, 0, 2),   # TEST-NET-1 documentation and examples
-            (198, 51, 100),# TEST-NET-2
-            (203, 0, 113), # TEST-NET-3
-            (192, 88, 99)  # Former IPv6 to IPv4 relay
-        ) or address[0] & 240 in ( # /4 subnets
-            224, # ip multiclass
-            240  # future use
-        ) or (address[0] == 100 and address[1] & 192 == 64 # /10 shared address space for carrier NAT
-        or address[0] == 172 and address[1] & 240 == 16    # /12 private network
-        or address[0] == 198 and address[1] & 254 == 18)   # /15 private network (benchmarking)
+    return address.is_global and not address.is_multicast
 
 def check_network(data: bytes) -> Mapping[str, List[bytes]]:
     """ Check for network indicators """
     return {
-            'ip': [ip for ip in re.findall(IPV4_RE, data) if valid_ipv4(ip) and not reserved_ipv4(ip)],
             'domain': re.findall(DOMAIN_RE, data, flags=re.IGNORECASE),
+            'ip': [ip for ip in re.findall(IP_RE, data) if is_public_ip4(ip)],
             'uri': re.findall(URI_RE, data, flags=re.IGNORECASE)
     }
---
+++ string_helper.py
@@ -0,0 +1,9 @@
+""" Helper functions for type coersion between string and bytes """
+
+def make_str(string: Union[str, bytes]) -> str:
+    """ Helper function for bytes to str coersion """
+    return string.decode(errors='ignore') if isinstance(string, bytes) else string
+
+def make_bytes(string: Union[str, bytes]) -> bytes:
+    """ Helper function for str to bytes coersion """
+    return string.encode(errors='ignore') if isinstance(string, str) else string
