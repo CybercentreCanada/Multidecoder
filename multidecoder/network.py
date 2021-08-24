ba7cbd507336e275a5069c7040c4536e784ffab7
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2021-08-24 12:19:12 -0700

    merge ioc.py into network.py, add email detection and tests

--- multidecoder/network.py
+++ multidecoder/network.py
@@ -1,30 +1,40 @@
-""" Network validation functions """
+""" Network indicators
+
+This module contains:
+- Regexes for finding IPs, domains, email addresses, and URLs
+- Validators for checking potential network indicators
+- find_network_indicators, a function for finding all network indicators for a text
+"""
+
+import re
 
 from ipaddress import AddressValueError, IPv4Address
-from typing import Union
+from typing import Mapping, Set, Union
 
 from multidecoder.domains import TOP_LEVEL_DOMAINS
 from multidecoder.string_helper import make_str, make_bytes
 
-def is_valid_domain(domain: Union[str, bytes]) -> bool:
-    """ Checks if a domain is valid """
-    parts = make_bytes(domain).rsplit(b'.', 1)
-    if len(parts) != 2:
-        return False
-    domain, top_level = parts
-    if top_level.upper() not in TOP_LEVEL_DOMAINS:
-        return False
-    return True
+IP_RE = rb'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+DOMAIN_RE = rb'(?i)\b(?:[a-z0-9-]+\.)+(?:xn--[a-z0-9]{4,18}|[a-z]{2,12})\b'
+EMAIL_RE = rb'(?i)\b[a-z0-9._%+-]{3,}@(' + DOMAIN_RE[4:] + rb')\b'
+URL_RE = rb'(?i)(?:ftp|http|https)://(' + IP_RE + rb'|' + DOMAIN_RE[4:] + rb')(?::[0-9]{1,5})?' \
+         rb'(?:/[a-z0-9/\-.&%$#=~?_+]{3,200})?'
 
-def is_valid_email(email: Union[str, bytes]) -> bool:
-    """ Checks if an email address is valid """
-    parts = make_bytes(email).split(b'@')
-    if len(parts) != 2:
-        return False
-    username, domain = parts
-    if not is_valid_domain(domain):
-        return False
-    return True
+def find_network_indicators(data: bytes) -> Mapping[str, Set[bytes]]:
+    """ Find network indicators in data
+
+    Args:
+        data: The data to search
+    """
+    return {
+            'domain': {domain for domain in re.findall(DOMAIN_RE, data)
+                       if is_valid_domain(domain)},
+            'email': {match.group(0) for match in re.finditer(EMAIL_RE, data)
+                      if is_valid_domain(match.group(1))},
+            'ip': {ip for ip in re.findall(IP_RE, data) if is_public_ip(ip)},
+            'url': {match.group(0) for match in re.finditer(URL_RE, data)
+                    if is_valid_domain(match.group(1)) or is_public_ip(match.group(1))}
+    }
 
 def is_public_ip(ip: Union[str, bytes]) -> bool:
     """
@@ -35,6 +45,11 @@ def is_public_ip(ip: Union[str, bytes]) -> bool:
         - a multicast address
         - a private address
         - a reserved address
+
+    Args:
+        ip: The ipv4 address to check
+    Returns:
+        Whether ip is a public ip address
     """
     try:
         address = IPv4Address(make_str(ip))
@@ -42,3 +57,20 @@ def is_public_ip(ip: Union[str, bytes]) -> bool:
         return False
     return address.is_global and not address.is_multicast
 
+def is_valid_domain(domain: Union[str, bytes]) -> bool:
+    """ Checks if a domain is valid
+
+    Checks the top level domain to ensure it is a registered top level domain
+
+    Args:
+        domain: The domain to validate
+    Returns:
+        Whether domain has a valid top level domain
+    """
+    parts = make_bytes(domain).rsplit(b'.', 1)
+    if len(parts) != 2:
+        return False
+    domain, top_level = parts
+    if top_level.upper() not in TOP_LEVEL_DOMAINS:
+        return False
+    return True
---
+++ test/test_network.py
@@ -0,0 +1,59 @@
+import re
+
+from multidecoder.network import *
+
+def test_ip_re():
+    assert re.match(IP_RE, b'8.8.8.8') # valid ip address
+    assert re.match(IP_RE, b'123.123.123.123') # up to three digits per group
+    assert re.match(IP_RE, b'103.245.67.89') # all digits can appear
+
+    # no more than 3 digits per group
+    assert not re.search(IP_RE, b'1234.8.8.8')
+    assert not re.search(IP_RE, b'8.1234.8.8')
+    assert not re.search(IP_RE, b'8.8.1234.8')
+    assert not re.search(IP_RE, b'8.8.8.1234')
+
+    # no extra . or missing numbers
+    assert not re.search(IP_RE, b'123..123.123.123')
+    assert not re.search(IP_RE, b'123.123..123.123')
+    assert not re.search(IP_RE, b'123.123.123..123')
+    assert not re.search(IP_RE, b'123.123.123.')
+    assert not re.search(IP_RE, b'.123.123.123')
+
+    # ip are found in context
+    assert re.search(IP_RE, b'http://8.8.8.8/something').group(0) == b'8.8.8.8'
+
+def test_domain_re():
+    # normal domain
+    assert re.match(DOMAIN_RE, b'www.google.com')
+    # internationalized domain name
+    assert re.match(DOMAIN_RE, b'xn--bcher-kva.example')
+    # internationalized top level domain
+    assert re.match(DOMAIN_RE, b'some.website.xn--4gbrim')
+
+def test_email_re():
+    assert re.match(EMAIL_RE, b'a_name@gmail.com')
+
+def test_url_re():
+    assert re.match(URL_RE, b'https://google.com')
+
+def test_is_valid_domain_re():
+    assert is_valid_domain(b'website.com')
+    assert not is_valid_domain(b'website.notatld')
+
+def test_is_public_ip():
+    assert is_public_ip(b'8.8.8.8')
+    assert not is_public_ip(b'0.0.0.0')
+    assert not is_public_ip(b'10.0.192.33')
+    assert not is_public_ip(b'127.0.0.1')
+
+def test_find_network_indicators():
+    assert find_network_indicators(
+        b'From: some_guy@suspicious.com,\n'
+        b'click this: https://8.8.8.8/gonna/steal/your/money.html  !!') \
+             == {
+                    'domain': {b'suspicious.com'},
+                    'email': {b'some_guy@suspicious.com'},
+                    'ip': {b'8.8.8.8'},
+                    'url': {b'https://8.8.8.8/gonna/steal/your/money.html'}
+                }
