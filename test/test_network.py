966fdced6435ae78b585da3ca1052795c9f4d8eb
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2022-02-14 15:24:40 -0800

    Extend IP detection

--- test/test_network.py
+++ test/test_network.py
@@ -4,7 +4,8 @@ from multidecoder.analyzers.network import *
 
 # IP --------------------------------------------
 def test_ip_re_matches_ips():
-    assert re.match(IP_RE, b'8.8.8.8') # valid ip address
+    assert re.match(IP_RE, b'127.0.0.1') # valid ip address
+    assert re.match(IP_RE, b'127.000.000.001') # full ip
     assert re.match(IP_RE, b'123.123.123.123') # up to three digits per group
     assert re.match(IP_RE, b'103.245.67.89') # all digits can appear
 
@@ -28,11 +29,25 @@ def test_ip_in_url():
     ip = re.search(IP_RE, b'http://8.8.8.8/something')
     assert ip and ip.group() == b'8.8.8.8'
 
+def test_ip_re_matches_octal():
+    assert re.match(IP_RE, b'0177.0.0.01')
+    assert re.match(IP_RE, b'00000000177.000.0.00000001')
+    assert re.match(IP_RE, b'0177.0.0.0000001')
+    assert re.match(IP_RE, b'000177.0000.00000.01')
+    assert re.match(IP_RE, b'0000177.000000000000000000.00000000000.00000000001')
+    assert re.match(IP_RE, b'00000000000000000000000000000000000000000000000000177.0.0.01')
+
+def test_ip_re_matches_hex():
+    assert re.match(IP_RE, b'0x7f.0x0.0x0.0x1')
+
+def test_ip_re_matches_mixed():
+    assert re.match(IP_RE, b'0xac.000000000000000000331.0246.174')
+
 def test_is_public_ip():
-    assert is_public_ip(b'8.8.8.8')
-    assert not is_public_ip(b'0.0.0.0')
-    assert not is_public_ip(b'10.0.192.33')
-    assert not is_public_ip(b'127.0.0.1')
+    assert parse_ip(b'8.8.8.8')
+    assert not parse_ip(b'0.0.0.0')
+    assert not parse_ip(b'10.0.192.33')
+    assert not parse_ip(b'127.0.0.1')
 
 # Domain ----------------------------------------
 
@@ -57,12 +72,49 @@ def test_find_domain_run():
 
 def test_find_domain_save():
     assert find_domains(b'oShLnk.Save') == []
+
 # Email -----------------------------------------
 
 def test_email_re():
     assert re.match(EMAIL_RE, b'a_name@gmail.com')
 
 # Url -------------------------------------------
+
 def test_url_re():
     assert re.match(URL_RE, b'https://google.com')
 
+def test_url_re_ip():
+    assert re.match(URL_RE, b'http://127.0.0.1')
+    assert re.match(URL_RE, b'http://127.000.000.001')
+
+def test_url_re_zero_suppresed_ip():
+    assert re.match(URL_RE, b'http://127.1')
+    assert re.match(URL_RE, b'http://192.168.1')
+    assert re.match(URL_RE, b'http://127.0.00000000000000000000000000000000001')
+
+def test_url_re_octal_ip():
+    assert re.match(URL_RE, b'http://0177.0.0.01')
+    assert re.match(URL_RE, b'http://00000000177.000.0.00000001')
+    assert re.match(URL_RE, b'http://0177.0.0.0000001')
+    assert re.match(URL_RE, b'http://000177.0000.00000.01')
+    assert re.match(URL_RE, b'http://0000177.000000000000000000.00000000000.00000000001')
+    assert re.match(URL_RE, b'http://00000000000000000000000000000000000000000000000000177.0.0.01')
+
+def test_url_re_hex_ip():
+    assert re.match(URL_RE, b'http://0x7f.0x0.0x0.0x1')
+    assert re.match(URL_RE, b'http://0x7f000001')
+
+def test_url_re_dword_ip():
+    assert re.match(URL_RE, b'http://2130706433')
+
+def test_url_re_mixed_ip():
+    assert re.match(URL_RE, b'http://00000000000000000000000000000000000000000000000000177.1')
+    assert re.match(URL_RE, b'http://0x7f.1')
+    assert re.match(URL_RE, b'http://127.0x1')
+    assert re.match(URL_RE, b'http://172.14263982')
+    assert re.match(URL_RE, b'http://0254.0xd9a6ae')
+    assert re.match(URL_RE, b'http://0xac.000000000000000000331.0246.174')
+    assert re.match(URL_RE, b'http://0331.14263982')
+
+def test_url_re_encoded_ip():
+    assert re.match(URL_RE, b'http://%31%32%37%2E%30%2E%30%2E%31')
