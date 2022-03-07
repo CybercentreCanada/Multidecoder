334a4de19ffe7dcc2f9f7d558fc96652f7385f09
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2022-03-07 11:06:32 -0500

    Fixing tests and linter fixes

--- test/test_multidecoder.py
+++ test/test_multidecoder.py
@@ -1,45 +1,54 @@
 import pytest
 
-from multidecoder import MultiDecoder
+from multidecoder.multidecoder import MultiDecoder
+
 
 @pytest.fixture
 def md():
     return MultiDecoder()
 
+
 def test_scan_empty(md):
     assert md.scan(b'') == []
+
 
 def test_analyze_data_url(md):
     assert md.scan(b'https://some.domain.com') == [
         {
+            'obfuscation': '',
             'type': 'network.url',
-            'value': b'https://some.domain.com',
-            'children': [
-                {
-                    'type': 'network.domain',
-                    'value': b'some.domain.com',
-                    'children': []
-                }
-            ]
-
+            'value': b'https://some.domain.com/',
+            'start': 0,
+            'end': 23,
+            'children': []
         }
     ]
+
 
 def test_scan_no_overlap(md):
     assert md.scan(b'google.com, amazon.com, 8.8.8.8') == [
         {
+            'obfuscation': '',
             'type': 'network.domain',
             'value': b'google.com',
+            'start': 0,
+            'end': 10,
             'children': []
         },
         {
+            'obfuscation': '',
             'type': 'network.domain',
             'value': b'amazon.com',
+            'start': 12,
+            'end': 22,
             'children': []
         },
         {
+            'obfuscation': '',
             'type': 'network.ip',
             'value': b'8.8.8.8',
+            'start': 24,
+            'end': 31,
             'children': []
-        },
+        }
     ]
