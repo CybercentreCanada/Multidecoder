7f0518ac305d9d806df053e813340f0bd375b021
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2022-03-11 09:30:55 -0500

    Refactor xml and add tests
    Added testing for xml escape characters
    refactored unescaping into its own function to make
    testing easier

--- multidecoder/analyzers/xml.py
+++ multidecoder/analyzers/xml.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
 import re
 
@@ -8,16 +9,16 @@ from multidecoder.registry import analyzer
 XML_ESCAPE_RE = rb'(?i)(?:&#(x[a-z0-9]{2}|(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}));){5,}'
 
 
+def unescape_xml(data: bytes) -> bytes:
+    return bytes(
+        int(x[1:], base=16) if x.startswith((b'x', b'X')) else int(x)
+        for x in data.replace(b'&#', b'').split(b';')[:-1]
+    )
+
+
 @analyzer('obfuscation.escape.xml')
 def find_xml_hex(data: bytes) -> list[Hit]:
     return [
-        Hit(
-            bytes(
-                int(x[1:], base=16) if x.startswith((b'x', b'X')) else int(x)
-                for x in match.group().replace(b'&#', b'').split(b';')[:-1]
-            ),
-            match.start(),
-            match.end(),
-            'escape.xml'
-        ) for match in re.finditer(XML_ESCAPE_RE, data)
+        Hit(unescape_xml(match.group()), match.start(), match.end(), 'escape.xml')
+        for match in re.finditer(XML_ESCAPE_RE, data)
     ]
