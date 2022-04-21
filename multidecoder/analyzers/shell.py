39f03eb11ce26dcd7005d4eadad37fa11a723760
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2022-04-21 13:36:40 -0400

    Shell improvements
    Added support for:
    - escaped quotes in cmd strings
    - correct handling of escaped caret ^^ and escaped newline ^\r\n by strip_carets
    - pwsh and carets in powershell name
    - detection and decoding of -EncodedCommand powershell argument

--- multidecoder/analyzers/shell.py
+++ multidecoder/analyzers/shell.py
@@ -1,21 +1,29 @@
 from __future__ import annotations
 
+import regex as re
+import binascii
+
+from multidecoder.analyzers.concat import DOUBLE_QUOTE_ESCAPES
 from multidecoder.hit import Hit, find_and_deobfuscate
 from multidecoder.registry import analyzer
 
-CMD_RE = rb'(?i)\bcmd[^,)"]+'
-POWERSHELL_RE = rb'(?i)"(\s*powershell[^"]+)"'
+
+CMD_RE = rb'(?i)\bc\^?m\^?d(?:' + DOUBLE_QUOTE_ESCAPES + rb'|[^)"])+'
+POWERSHELL_RE = rb'(?i)"([\s^]*p^?(?:o^?w^?e^?r^?s^?h^?e^?l^?l|w^?s^?h)[^"]+)"'
 SH_RE = rb'"(\s*(?:sh|bash|zsh|csh)[^"]+)"'
+ENC_RE = rb'(?i)(?:-|/)e(?:n(?:c(?:o(?:d(?:e(?:d(?:c(?:o(?:m(?:m(?:a(?:nd?)?)?)?)?)?)?)?)?)?)?)?|c)' \
+         rb'?[\s]+([a-z0-9+/]{4,}=?=?)'
 
 
-def strip_carets(text: bytes):
-    return bytes(i for i in text if i != ord('^'))
+def strip_carets(cmd: bytes) -> bytes:
+    return re.sub(rb'(\^+)(/r/n)?',
+                  lambda match: b'^' * (len(match.group(1)) // 2),
+                  cmd)
 
 
 def deobfuscate_cmd(cmd: bytes):
-    if b'^' in cmd:
-        return strip_carets(cmd), 'unescape.shell.carets'
-    return cmd, ''
+    stripped = strip_carets(cmd)
+    return stripped, 'unescape.shell.carets' if stripped != cmd else ''
 
 
 @analyzer('shell.cmd')
@@ -25,4 +33,28 @@ def find_cmd_strings(data: bytes) -> list[Hit]:
 
 @analyzer('shell.powershell')
 def find_powershell_strings(data: bytes) -> list[Hit]:
-    return find_and_deobfuscate(POWERSHELL_RE, data, deobfuscate_cmd, group=1)
+    out = []
+    for match in re.finditer(POWERSHELL_RE, data):
+        obfuscation = []
+        deobfuscated, ob = deobfuscate_cmd(match.group(1))
+        if ob:
+            obfuscation.append(ob)
+        enc = re.search(ENC_RE, deobfuscated)
+        if enc:
+            b64 = (binascii.a2b_base64(_pad(enc.group(1)))
+                           .decode('utf-16', errors='ignore')
+                           .encode())
+            deobfuscated = deobfuscated[:enc.start()] + b64 + deobfuscated[enc.end():]
+            obfuscation.append('powershell.base64')
+        out.append(Hit(deobfuscated, '/>'.join(obfuscation), match.start(), match.end()))
+    return out
+
+
+def _pad(b64: bytes) -> bytes:
+    padding = -len(b64) % 4
+    if padding == 3:
+        return b64[:-1]  # Corrupted end, just keep the valid part
+    elif padding:
+        return b64 + b'='*padding
+    else:
+        return b64
