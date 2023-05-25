663a6f65725723825d545edf5bc6724f4e056218
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2023-05-25 16:42:36 -0400

    fix indentation

--- tests/test_analyzers/test_javascript.py
+++ tests/test_analyzers/test_javascript.py
@@ -3,4 +3,6 @@ from multidecoder.hit import Hit
 
 
 def test_find_unescape():
-    assert find_unescape(b"unescape('help%20Im%20stuck%20in%20a%20url%20factory!')") == [Hit(b'help Im stuck in a url factory!', 'function.unescape', 0, 55)]
+    assert find_unescape(b"unescape('help%20Im%20stuck%20in%20a%20url%20factory!')") == [
+        Hit(b'help Im stuck in a url factory!', 'function.unescape', 0, 55)
+    ]
