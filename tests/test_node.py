b17e04cb3d4def6d6c3af0765a05cf683055bb48
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2023-10-11 12:28:45 -0400

    Depricate query (#59)
    * Depricate invert_tree, squash_replace, and obfuscation_counts
    
    These methods are still based on the tree as a list of children
    model and are made irrelevant by new node methods. Since node
    is now iterable, invert_tree can be replaced by list(node).
    obfuscation_counts can be replaced with:
    Counter(child.obfuscation for node in node).
    Neither require a dedicated function anymore.
    Since trees now have a root node squash_replace can now be a
    method of Node with no other arguments, this is implimented as
    Node.flatten().
    
    * Add tests for Node.flatten() and fix comparison.
    
    * Add Node.flatten() test for full overlap

---
+++ tests/test_node.py
@@ -0,0 +1,33 @@
+import pytest
+from multidecoder.node import Node
+
+DATA1 = b"12345HERE WE ARE54321"
+EXAMPLE1 = Node(
+    "",
+    DATA1,
+    "",
+    0,
+    len(DATA1),
+    children=[Node("test", b"And now for something completely different.", "different", 5, 16)],
+)
+
+
+def test_original_no_parent():
+    data = b"unmodified data"
+    assert Node("test", data, "", 0, len(data)).original == data
+
+
+def test_original_parent():
+    assert EXAMPLE1.children[0].original == b"HERE WE ARE"
+
+
+@pytest.mark.parametrize(
+    ("node", "flat"),
+    [
+        (EXAMPLE1, b"12345And now for something completely different.54321"),
+        (Node("", b"abc", "", 0, 3, children=[Node("", b"FIRST", "", 0, 2), Node("", b"SECOND", "", 1, 3)]), b"FIRSTc"),
+        (Node("", b"abc", "", 0, 3, children=[Node("", b"FIRST", "", 0, 3), Node("", b"SECOND", "", 0, 3)]), b"FIRST"),
+    ],
+)
+def test_flatten(node, flat):
+    assert node.flatten() == flat
