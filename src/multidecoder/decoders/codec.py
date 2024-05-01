df25d36f3db1b78205d824437c9e1d4d75189657
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2024-05-01 16:03:29 -0400

    Allow utf-16 to grab null terminated string sections all at once.

--- src/multidecoder/decoders/codec.py
+++ src/multidecoder/decoders/codec.py
@@ -9,7 +9,9 @@ import regex as re
 from multidecoder.node import Node
 from multidecoder.registry import decoder
 
-UTF16_RE = rb"(?s)(?:[^\x00-\x08\x0e-\x1f\x7f-\x9f]\x00){7,}"
+UTF16_RE = (
+    rb"(?s)(?:[^\x00-\x08\x0e-\x1f\x7f-\x9f]\x00){7,}(?:\x00\x00(?:\x00\x00)?[^\x00-\x08\x0e-\x1f\x7f-\x9f]\x00){7,})*"
+)
 
 
 @decoder
