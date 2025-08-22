a3507a651d0361106fb40ecb9cbfde4b37511d33
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2025-08-22 16:08:11 +0000

    question.name domain fpos

--- tests/test_decoders/test_network.py
+++ tests/test_decoders/test_network.py
@@ -215,6 +215,7 @@ def test_DOMAIN_RE_context(data, domain):
         b"mman.cc",
         b"utlook.com",
         b"download.show",
+        b"question.name",
     ],
 )
 def test_domain_is_false_positive(domain):
